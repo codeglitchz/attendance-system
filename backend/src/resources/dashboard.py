c82a8d75375069b15e0b827608209bfacb67cde7
mdmujtabaraza <45493966+mdmujtabaraza@users.noreply.github.com>
2021-08-31 18:35:22 +0530

    Removed jwt_optional import from jwt_extended

--- backend/src/resources/dashboard.py
+++ backend/src/resources/dashboard.py
@@ -1,12 +1,12 @@
 from flask_restful import Resource
-from flask_jwt_extended import jwt_optional, get_jwt_identity
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from src.libs.strings import gettext
 
 
 class Dashboard(Resource):
     @classmethod
-    @jwt_optional
+    @jwt_required(optional=True)
     def get(cls):
         user_id = get_jwt_identity()
         if user_id:
